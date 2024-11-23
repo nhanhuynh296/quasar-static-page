3696372837597293f04f8c6d1bf02fcf7f36ca62
Nathan.Huynh <nathan@Nathans-MacBook-Pro.local>
2024-11-24 00:03:49 +1300

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-page class="row justify-center q-px-md" style="background: #f5c2e7">
+    <div class=" col-md-6 col-xs-12">
+
+      <h2 class="text-h4 text-center ">Date proposal</h2>
+
+      <div class="text-body1 ">
+
+
+        <p class="text-body1">
+          Dear my dearest Claudia,
+        </p>
+
+        <p class="text-body1">
+          I hope this email find you before me. I would like to propose a date on December 1st,
+          2024. This date would last from brunch time (12-ish) till late 9:00.
+          My plan consists of us walking together through the city.
+        </p>
+
+        <p class="text-body1">
+          In terms of feasting, I believe that it would be best for us to find two restaurants on our walk: one that I
+          find
+          interesting, and one that you find interesting. We will eat at one restaurant for lunch and one for dinner. I
+          hope
+          this term is amicable.
+        </p>
+
+        <p class="text-body1">
+          Next, our activities would include: walking the city to take pictures and see the old architecture. There is
+          also
+          a
+          cute outdoor art exhibit that I think would be fun to explore. But most importantly, this is a date to find
+          out
+          more
+          about each other.
+        </p>
+
+        <p class="text-body1">
+          I would also wanted to propose a pool competition between us, i think it would be an opportunity for us to
+          really
+          getting to really know each other
+        </p>
+
+        <p class="text-body1">
+          I eagerly long for your response, ma belle.
+        </p>
+
+
+        <p>
+          Sincerely,
+          <br/>
+          Your dearest bestie
+          <br/>
+          Nathan Huynh
+        </p>
+
+
+      </div>
+    </div>
+
+
+  </q-page>
+</template>
+
+<script lang="ts" setup>
+</script>
+<style scoped>
+</style>
